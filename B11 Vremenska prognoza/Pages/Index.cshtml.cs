566dc948a66b566fae5f461c5e8225f75d5af2af
svi <svi@DESKTOP-RKLK4N1>
2024-06-05 23:52:23 +0200

    Add project files.

---
+++ B11 Vremenska prognoza/Pages/Index.cshtml.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace B11_Vremenska_prognoza.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+        public string Grad { get; set; }
+        public string Link { get; set; }
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {}
+
+    }
+}
